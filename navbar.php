0b2d5f53a575a02bda187185c5a9320a1bb4bec7
Rosyid Fadillah  Nurrokhim <79777623+rosyidfadillah01@users.noreply.github.com>
2025-03-14 06:57:05 +0700

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,18 @@
+<!-- Responsive navbar-->
+<nav id="navbar" class="navbar fixed-top navbar-expand-lg bg-light shadow-lg" >
+    <div class="container px-2" >
+        <img src="img/logo_loundry2.png" width="70px" alt="">
+        <a class="navbar-brand fw-bold fs-5" style="color:#9d3b8b;" href="index.php">Loundry | Beranda</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item"><a class="nav-link active" aria-current="page" href="#beranda">Beranda</a></li>
+                <li class="nav-item"><a class="nav-link" href="#tentang">Tentang</a></li>
+                <li class="nav-item"><a class="nav-link" href="#layanan">Layanan</a></li>
+                <li class="nav-item"><a class="nav-link" href="#harga">Harga</a></li>
+                <li class="nav-item"><a class="nav-link" href="#kontak">Kontak</a></li>
+                <li class="nav-item mx-3"><a class="btn btn-purple shadow-sm " href="login.php" id="loginBtn">Login</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
